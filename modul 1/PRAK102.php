7b4ca39cf3a565b72fc47460c576767692ff2025
Ryuzora <ryuzora8@gmail.com>
2026-04-08 18:04:26 +0800

    change printf to echo

--- modul 1/PRAK102.php
+++ modul 1/PRAK102.php
@@ -3,5 +3,6 @@
 $tinggi = 5.4;
 $panjang = 8.9;
 $lebar = 14.7;
+$volume = 1/3 * $panjang * $lebar * $tinggi;
 
-printf("%.3f m3", 1/3 * $panjang * $lebar * $tinggi);
+echo number_format($volume, 4) . " m3";
--- modul 1/PRAK103.php
+++ modul 1/PRAK103.php
@@ -4,7 +4,6 @@ $fahrenheit = ($celcius * (9/5)) + 32;
 $reamur = $celcius * (4/5);
 $kelvin = $celcius + 273.15;
 
-printf("Celsius = %.4f\n\n", $celcius);
-printf("Fahrenheit (F) = %.4f\n", $fahrenheit);
-printf("Reamur (R) = %.4f\n", $reamur);
-printf("Kelvin (K) = %.4f\n", $kelvin);
+echo "Fahrenheit (F) = " . number_format($fahrenheit, 4) . "\n";
+echo "Reamur (R) =  = " . number_format($reamur, 4) . "\n";
+echo "Kelvin (K) = " . number_format($kelvin, 4);
